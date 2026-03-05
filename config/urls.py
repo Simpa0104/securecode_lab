96aafa3bfadf050a7031d654bb076a646977c9b3
Simpa0104 <Steventorres042001@gmail.com>
2026-03-05 17:48:07 -0500

    Segundo commit

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import home
+from users.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', home, name='home'),
+    path('register/', register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from .models import Profile
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            Profile.objects.create(
+                user=user,
+                role="student",
+            )
+
+            return redirect('login')
+
+        else:
+            form = RegisterForm()
+
+        return render(request, 'registration/register.html', {'form': form})
